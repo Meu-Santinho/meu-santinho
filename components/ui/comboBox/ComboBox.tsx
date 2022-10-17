df65ea00544189d020d186485338d0310c4ef4bf
marianacoimbra <mariana.coimbra@ccc.ufcg.edu.br>
2022-10-16 21:45:18 -0300

    remove static combobox options

--- components/ui/comboBox/ComboBox.tsx
+++ components/ui/comboBox/ComboBox.tsx
@@ -71,7 +71,6 @@ export default function ComboBoxSelect(props: {
             </Combobox.Button>
             {candidatosFiltrados.length > 0 && (isOpen || open) && (
               <Combobox.Options
-                static
                 class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
               >
                 {/* @ts-expect-error: erro */}
